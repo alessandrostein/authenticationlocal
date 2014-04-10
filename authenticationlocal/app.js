3f53e8b7c99df4f0a966e80d80ee8d7695282a9c
Alessandro Stein <alessandrostein@github.com>
2014-04-09 21:52:26 -0300

    update app.js

--- authenticationlocal/app.js
+++ authenticationlocal/app.js
@@ -68,5 +68,19 @@ app.get('/login', function(req, res){
     res.sendfile('views/login.html');
 });
 
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
+
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+
+app.get('/loginSuccess', function(req, res, next) {
+  res.send('Successfully authenticated');
+});
 
 module.exports = app;
